f4eb9c9a1572ecb102791ae2743a3cdfbe6f726e
Kiana Long <kianajade42.kl@gmail.com>
2021-01-19 00:46:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if  greeting == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
 if greeting != greeting.upcase
-  return  "HUH?! SPEAK UP,SONNY!"
+  return  "HUH?! SPEAK UP, SONNY!"
 else
   return "NO, NOT SINCE 1938!"
 end
